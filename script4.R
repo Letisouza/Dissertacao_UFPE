5bc415dd16d91cee9602a05996168fecf0e68137
Letisouza <leticiasouzaufpe@gmail.com>
2022-09-08 12:34:02 -0300

    08/09/2022

--- script4.R
+++ script4.R
@@ -3,12 +3,16 @@ library(tidyverse)
 gdp <- readRDS("gdp_europe_final.rds")
 
 per_country <- gdp %>%
-  filter(!leader %in% c("Petr Necas","Bohuslav Sobotka", "Andrius Kubilius", "Algirdas Butkevicius", "Beate Szydlo", "Donald Tusk", "Emil Boc", "Viktor Ponta", "Dacian Ciolos")) %>% 
+  filter(!leader %in% c("Petr Necas","Bohuslav Sobotka", "Andrius Kubilius", "Algirdas Butkevicius", "Beate Szydlo", 
+                        "Donald Tusk", "Emil Boc", "Viktor Ponta", "Dacian Ciolos")) %>% 
   group_by(country, leader,yearbegin, yearend, lr) %>% 
   summarise(totalaverage = (unique(totalaverage))) %>% 
   arrange(country, yearbegin) %>% 
   filter(yearend >= 2012,
          yearbegin <= 2020)
+
+# These leaders took off are coded both as president or prime-minister along another leaders in the same years in the same country. 
+# I made the decision to choose the leaders who cover most time during the time of analysis, since the leader isn't important here.
 
 base <- as.data.frame(per_country)
 
@@ -20,10 +24,20 @@ saveRDS(base, "base.rds")
 
 ------
   
-base <- readRDS("base.rds")  
-  
+  base <- readRDS("base.rds")  
+
 # cases: Hungary and Czechia
 
-ggplot(base,
-  aes(formalnotice, totalaverage)) +
-  geom_point()
+g1 <- ggplot(base,
+             aes(formalnotice, totalaverage, label=country)) +
+  geom_jitter(width = 0.08)+
+  theme_classic()+
+  scale_y_continuous(breaks=seq(0, 1, by = .10)) +
+  scale_x_continuous(breaks=seq(0, 15, by = 2)) +
+  labs(title="Relação entre Populismo e Infrações a \nLeis sobre Migração na UE", 
+       y="Nível de Populismo", 
+       x="Procedimentos de Infração",
+       caption = "Fonte: GDP e UE") +
+  geom_text(aes(label=ifelse(totalaverage > 0.8 | formalnotice > 10, country,'')),hjust=0.6,vjust=-0.4)
+
+g1
