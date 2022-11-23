1fa377f47e835e66bde4d65a78c286cb1588f463
Letisouza <leticiasouzaufpe@gmail.com>
2022-11-23 14:27:28 -0300

    23/11/2022

--- script4.R
+++ script4.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+#############
 gdp <- readRDS("gdp_europe_final.rds")
 
 per_country <- gdp %>%
@@ -23,9 +24,11 @@ base <- cbind(base, formalnotice, ref)
 
 saveRDS(base, "base.rds")
 
+##############
+
 base <- readRDS("base.rds")  
 
-# EXCLUIR titulo: Relação entre Populismo e Infrações a Leis sobre Migração na UE
+# graph: populism x infractions
 
 theme_set(theme_bw())
 
@@ -50,6 +53,90 @@ g1 <- ggplot(base,
 g1
 
 ggsave(g1, filename = "g1.jpeg", width = 8, height = 5)
+
+
+#######
+
+library(readxl)
+
+migration1 <- read_xlsx("migration1.xlsx")
+
+# transforming to a data.frame
+
+df1 <- data.frame(matrix(unlist(migration1), nrow=length(migration1), byrow=T),stringsAsFactors=F)
+
+# turning into a long form
+
+df2 <- as.data.frame(t(df1))
+
+# changing the type of the variables to numeric
+
+df2$V4 <- as.numeric(df2$V4)
+df2$V5 <- as.numeric(df2$V5)
+df2$V6 <- as.numeric(df2$V6)
+df2$V7 <- as.numeric(df2$V7)
+
+
+# changing the rows with "present" to 2022, even tho it doesn't mean this year is really 
+# the end of the term. 
+
+library(dplyr)
+
+migration2 <- df2 %>% 
+  mutate_at(vars(V5, V7), funs(round(., 1))) %>% 
+  rename("country" = V1,
+         "ref" = V2,
+         "year" = V3,
+         "migr_stock" = V4,
+         "migr_stock_pop_perc" = V5,
+         "refugee_stock" = V6,
+         "refugee_stock_migr_perc" = V7)
+
+saveRDS(migration2, "migration2.rds")
+
+############
+
+# graph: migration
+
+migration <- readRDS("migration2.rds")
+
+theme_set(theme_bw())
+
+g2 <- ggplot(migration, aes(ref, migr_stock_pop_perc, fill = year)) +
+  geom_bar(stat = "identity",
+           position = position_dodge()) +
+  theme(panel.grid = element_blank()) +
+  scale_y_continuous(breaks=seq(0, 20, by = 5)) +
+  labs(y="Immigrant Stock (% of population) ", 
+       x="",
+       fill = "",
+       caption = "Source: UN DESA") +
+  theme(axis.title = element_text(size = 10.5),
+        axis.text = element_text(size = 9)) +
+  scale_fill_manual(breaks = c("2015", "2019", "2020"),
+                    values = c("aquamarine2", "darkgoldenrod1", "bisque4"))
+
+g2
+
+ggsave(g2, filename = "g2.jpeg", width = 10, height = 5)
+
+g3 <- ggplot(migration, aes(ref, refugee_stock_migr_perc, fill = year)) +
+  geom_bar(stat = "identity",
+           position = position_dodge()) +
+  theme(panel.grid = element_blank()) +
+  scale_y_continuous(breaks=seq(0, 20, by = 5)) +
+  labs(y="Refugee Stock (% of immigrants) ", 
+       x="",
+       fill = "",
+       caption = "Source: UN DESA") +
+  theme(axis.title = element_text(size = 10.5),
+        axis.text = element_text(size = 9)) +
+  scale_fill_manual(breaks = c("2015", "2019", "2020"),
+                    values = c("aquamarine2", "darkgoldenrod1", "bisque4"))
+
+g3
+
+ggsave(g3, filename = "g3.jpeg", width = 10, height = 5)
 
 # cases: Hungary and Czechia
 
